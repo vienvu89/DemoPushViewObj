1129d1c968b8a9b6baaa3211736527cd1cf19e86
Vien Vu <lamvienbk@gmail.com>
2015-12-10 16:37:59 +0700

    Initial Commit

---
+++ DemoPushViewObj/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DemoPushViewObj
+//
+//  Created by Vien Vu  on 12/10/15.
+//  Copyright © 2015 VienVu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DemoPushViewObj/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DemoPushViewObj
+//
+//  Created by Vien Vu  on 12/10/15.
+//  Copyright © 2015 VienVu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
